ca45b5c4aca6c29b2979fa202a1e3d5443f7a17f
Denis Pavlenko <den4ikpavlenya@gmail.com>
2024-03-12 13:53:14 +0300

    push project

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+  movies: ['Логан', 'Лига справедливости', 'Ла-ла лэнд', 'Одержимость', 'Скотт Пилигрим против...'],
+};
+
+const adv = document.querySelectorAll('.promo__adv img'),
+  poster = document.querySelector('.promo__bg'),
+  genre = poster.querySelector('.promo__genre'),
+  movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach((item) => {
+  item.remove(); //удаляет каждый элемент класса
+});
+
+genre.textContent = 'драма'; //меняет текст на драма
+
+poster.style.backgroundImage = 'url("img/bg.jpg")'; //изменяет фон, внутри только двойные кавычки
+
+movieList.innerHTML = ''; //очищение элемента, т.е. удаление содержимого
+
+movieDB.movies.sort(); //сортирует содержимое массива movies по алфавиту
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
